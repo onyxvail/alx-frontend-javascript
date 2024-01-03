c46184eb9db9c6c913d212859876b8c532ea8ec4
onyxvail <ons.hfi@gmail.com>
2024-01-03 14:50:18 +0100

    fixing error

---
+++ 0x00-ES6_basic/6-main.js
@@ -0,0 +1,4 @@
+// Updated the import statement in 6-main.js
+import getSanFranciscoDescription from './6-string-interpolation';
+
+console.log(getSanFranciscoDescription());
---
+++ 0x00-ES6_basic/jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/'],
+  };
+  
