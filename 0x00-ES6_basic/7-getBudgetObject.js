f01344a242a5a134f3c0c127932010e3470ff44e
onyxvail <ons.hfi@gmail.com>
2024-01-03 14:56:13 +0100

    7. Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+// the fn is modified via shorthand syntax
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+  return budget;
+}
