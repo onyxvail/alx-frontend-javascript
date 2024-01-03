53919bd84c3b5ce3984d6db67b61bdf40e3a0f01
onyxvail <ons.hfi@gmail.com>
2024-01-03 18:02:27 +0100

    ADVANCED

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,22 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
