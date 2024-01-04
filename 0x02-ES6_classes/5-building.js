09ef6617f82e1a6efae1ef22be7e3ad175835169
onyxvail <ons.hfi@gmail.com>
2024-01-04 18:17:54 +0100

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('evacuationWarningMessage must be overridden in the subclass');
+  }
+}
