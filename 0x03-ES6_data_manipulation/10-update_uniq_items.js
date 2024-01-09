48b69acdf33bf2e8368e270acad4004b062dd849
onyxvail <ons.hfi@gmail.com>
2024-01-09 14:07:43 +0100

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+  map.forEach((val, key) => {
+    if (val === 1) map.set(key, 100);
+  });
+
+  return map;
+}
