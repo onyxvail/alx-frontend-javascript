23779508d8e28a2a93bf8e5c0e8b4231c81b2886
onyxvail <ons.hfi@gmail.com>
2024-01-09 14:19:35 +0100

    ADVANCED-ERROR FIXED

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,13 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
+function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
 
-  if (count >= 5) throw new Error('Endpoint load is high');
+  if (count >= 4) {
+    throw new Error('Endpoint load is high');
+  }
 
   weakMap.set(endpoint, count + 1);
 }
+
+export { queryAPI, weakMap };
