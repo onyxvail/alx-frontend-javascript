be5c6c82ecd556d98969d83749fb0c2baa273dcb
onyxvail <ons.hfi@gmail.com>
2024-01-09 14:17:04 +0100

    fixing error

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,19 @@
 export default function groceriesList() {
-  return new Map([
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ]);
+  const groceryMap = new Map();
+
+  const groceryItems = [
+    { name: 'Apples', quantity: 10 },
+    { name: 'Tomatoes', quantity: 10 },
+    { name: 'Pasta', quantity: 1 },
+    { name: 'Rice', quantity: 1 },
+    { name: 'Banana', quantity: 5 },
+  ];
+
+  groceryItems.forEach((item) => {
+    const { name, quantity } = item;
+
+    groceryMap.set(name, quantity);
+  });
+
+  return groceryMap;
 }
