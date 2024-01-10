448a7a27053f147541c67199032781ce7f55a276
onyxvail <ons.hfi@gmail.com>
2024-01-10 17:52:01 +0100

    task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,19 +1,22 @@
 interface Teacher {
-    firstName: string;
-    lastName: string;
-    readonly fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    readonly location: string;
-    [key: string]: any;
-  }
-  
-  const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-  };
-  
-  console.log(teacher3);
-  
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
